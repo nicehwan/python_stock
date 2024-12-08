1a8d106e39d2cfe91b03c865eba0e303693c7311
augustine <nicehwan@gmail.com>
2024-12-08 16:02:36 +0900

    samsung microsfot stock change

--- main.py
+++ main.py
@@ -12,11 +12,19 @@ msft = yf.download('MSFT', start='2018-05-04')
 #
 # plt.plot(sec.index, sec.Close, 'b', label='Samsung Electronics')
 # plt.plot(msft.index, msft.Close, 'r--', label='Mircosoft')
-plt.legend(loc='best')
 #plt.show()
 
 sec_dpc = (sec['Close']-sec['Close'].shift(1)) / sec['Close'].shift(1) * 100
 sec_dpc.iloc[0] = 0
-plt.hist(sec_dpc, bins=50)
+sec_dpc_cs = sec_dpc.cumsum()
+
+msft_dpc = (msft['Close']-msft['Close'].shift(1)) / msft['Close'].shift(1) * 100
+msft_dpc.iloc[0] = 0
+msft_dpc_cs = msft_dpc.cumsum()
+
+plt.plot(sec.index, sec_dpc_cs, 'b', label = 'Samsung')
+plt.plot(msft.index, msft_dpc_cs, 'r', label = 'Microsoft')
+plt.ylabel('Change %')
 plt.grid(True)
+plt.legend(loc='best')
 plt.show()
