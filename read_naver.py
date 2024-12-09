2105b22e1c86119efa951fb2f00a68876b394c27
augustine <nicehwan@gmail.com>
2024-12-09 22:01:28 +0900

    example reading naver site

--- read_naver.py
+++ read_naver.py
@@ -1,8 +1,33 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+# url = 'https://finance.naver.com/item/frgn.naver?code=334890'
+# with urlopen(url) as doc:
+#     html = BeautifulSoup(doc, 'lxml')
+#     print(html)
+#     pgrr = html.find('td', class_='pgRR')
+#
+#     if pgrr and pgrr.a:
+#         print(pgrr.a['href'])
+#     else:
+#         print("Element not found")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
 
 url = 'https://finance.naver.com/item/frgn.naver?code=334890'
-with urlopen(url) as doc:
-    html = BeautifulSoup(doc, 'lxml')
-    pgrr = html.find('td', class_='pgRR')
+driver.get(url)
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'lxml')
+
+pgrr = soup.find('td', class_='pgRR')
+if pgrr and pgrr.a:
     print(pgrr.a['href'])
+else:
+    print("Element not found")
+
+driver.quit()
